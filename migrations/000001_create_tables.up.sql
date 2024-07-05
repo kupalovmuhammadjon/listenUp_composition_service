82ad07f5d7118d59b4c837164ea4a9f6818f10ed
Umaroff07 <ibrohimumarov0702@gmail.com>
2024-07-05 11:30:25 +0500

    submodule added

--- migrations/000001_create_tables.up.sql
+++ migrations/000001_create_tables.up.sql
@@ -20,5 +20,17 @@ CREATE TABLE episodes (
       duration float,
       created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP not null,
       updated_at TIMESTAMP,
-      deleted_at TIMESTAMP
+      deleted_at TIMESTAMP,
+      search_vector tsvector
 );
+
+
+CREATE OR REPLACE FUNCTION update_search_vector() RETURNS TRIGGER AS $$
+BEGIN
+  NEW.search_vector := to_tsvector('english', coalesce(NEW.title, '') || ' ');
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER tsvectorupdate BEFORE INSERT OR UPDATE
+ON episodes FOR EACH ROW EXECUTE FUNCTION update_search_vector();
